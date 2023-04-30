ec40317c489244bda69d6bdc9ee02fee3595e637
farhan-abdullah <farhan.abdullah94@gmail.com>
2023-04-30 19:21:01 +0200

    Get dynamically data 63-2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const categories = require('./Data/categories.json');
+const news = require('./Data/news.json');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,23 @@ app.get('/', (req, res) => {
 });
 app.get('/categories', (req, res) => {
 	res.send(categories);
+});
+app.get('/categories/:id', (req, res) => {
+	const id1 = parseInt(req.params.id);
+	if (id1 == 0) {
+		res.send(news);
+	} else {
+		const selectedCategory = news.filter((n) => parseInt(n.category_id) === id1);
+		res.send(selectedCategory);
+	}
+});
+app.get('/news', (req, res) => {
+	res.send(news);
+});
+app.get('/news/:id', (req, res) => {
+	const id1 = req.params.id;
+	const selectedNews = news.find((n) => n._id === id1);
+	res.send(selectedNews);
 });
 
 app.listen(port, () => {
